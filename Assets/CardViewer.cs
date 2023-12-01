66dc312efaa46aebfbbf58b033bb0398c678b78d
Loenna <ginzzang1010@naver.com>
2023-12-01 18:09:18 +0900

    1차 작업

--- Assets/CardViewer.cs
+++ Assets/CardViewer.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class CardViewer : MonoBehaviour
 {
-    public Text nameText, levelText;
+    public Text NameText;
+    public Text LevelText;
     public Image Image;
     
-    void Start()
+    void Awake()
     {
-        
+        NameText = transform.Find("Name")?.GetComponent<Text>();
+        LevelText = transform.Find("Level")?.GetComponent<Text>();
+        Image = transform.Find("Image")?.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Set(string name, int level, string image)
     {
-        
+        NameText.text = name;
+        LevelText.text = $"Lv {level}";
+
+        Image.sprite = Resources.Load<Sprite>($"Images/{image}");
     }
 }
